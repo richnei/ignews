b93e372b09d74d12f15b8dc75ce007f1e97f88d1
Richnei <rich.nei@hotmail.com>
2022-04-05 19:25:13 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        HelloWorld
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9,99 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
